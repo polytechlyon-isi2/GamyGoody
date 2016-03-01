27efc1631583d8fbf19b98a6fd3fed1dba75fc40
Etienne DEBARD <etienne.debard@etu.univ-lyon1.fr>
2016-03-01 18:23:01 +0100

    ajout des objets game / categorie

---
+++ src/DAO/GameDAO.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace GamyGoody\DAO;
+
+use GamyGoody\Domain\Game;
+
+class GameDAO extends DAO
+{
+    /**
+     * Return a list of all games, sorted by date (most recent first).
+     *
+     * @return array A list of all games.
+     */
+    public function findAll() {
+        $sql = "select * from game order by game_id desc";
+        $result = $this->getDb()->fetchAll($sql);
+
+        // Convert query result to an array of domain objects
+        $games = array();
+        foreach ($result as $row) {
+            $gameId = $row['game_id'];
+            $games[$gameId] = $this->buildDomainObject($row);
+        }
+        return $games;
+    }
+
+    /**
+     * Creates an game object based on a DB row.
+     *
+     * @param array $row The DB row containing game data.
+     * @return \MicroCMS\Domain\game
+     */
+    protected function buildDomainObject($row) {
+        $game = new Game();
+        $game->setId($row['game_id']);
+        $game->setTitle($row['game_title']);
+        return $game;
+    }
+
+    public function find($id) {
+        $sql = "select * from game where game_id=?";
+        $row = $this->getDb()->fetchAssoc($sql, array($id));
+
+        if ($row)
+            return $this->buildDomainObject($row);
+        else
+            throw new \Exception("No game matching id " . $id);
+    }
+
+    /**
+     * Saves an game into the database.
+     *
+     * @param \MicroCMS\Domain\game $game The game to save
+     */
+    public function save(game $game) {
+        $gameData = array(
+            'game_title' => $game->getTitle()
+            );
+
+        if ($game->getId()) {
+            // The game has already been saved : update it
+            $this->getDb()->update('game', $gameData, array('game_id' => $game->getId()));
+        } else {
+            // The game has never been saved : insert it
+            $this->getDb()->insert('game', $gameData);
+            // Get the id of the newly created game and set it on the entity.
+            $id = $this->getDb()->lastInsertId();
+            $game->setId($id);
+        }
+    }
+
+    /**
+     * Removes an game from the database.
+     *
+     * @param integer $id The game id.
+     */
+    public function delete($id) {
+        // Delete the game
+        $this->getDb()->delete('game', array('game_id' => $id));
+    }
+}
---
+++ src/Domain/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace GamyGoody\Domain;
+
+class Category 
+{
+    /**
+     * Category id.
+     *
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * Category title.
+     *
+     * @var string
+     */
+    private $title;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+}
